aa0474560a7fe31e7e323b95c07f4da173c168c1
Tony Liu <tonyli@orionhealth.com>
2018-12-03 09:37:13 +1300

    day2

--- day1/test.js
+++ day1/test.js
@@ -4,33 +4,33 @@ const solution = require("./solution");
 describe("Day 1: Chronal Calibration", () => {
   describe("Part 1", () => {
     it("Default to 0", () => {
-      assert.equal(0, solution.part1([]));
+      assert.equal(solution.part1([]), 0);
     });
 
     it("Adds numbers", () => {
-      assert.equal(4, solution.part1([0, 1, 2, 1]));
+      assert.equal(solution.part1([0, 1, 2, 1]), 4);
     });
 
     it("Works with negatives", () => {
-      assert.equal(-2, solution.part1([-1, 2, -3]));
+      assert.equal(solution.part1([-1, 2, -3]), -2);
     });
   });
 
   describe("Part Two", () => {
     it("First reaches 0 twice", () => {
-      assert.equal(0, solution.part2([1, -1]));
+      assert.equal(solution.part2([1, -1]), 0);
     });
 
     it("First reaches 10 twice", () => {
-      assert.equal(10, solution.part2([3, 3, 4, -2, -4]));
+      assert.equal(solution.part2([3, 3, 4, -2, -4]), 10);
     });
 
     it("First reaches 5 twice", () => {
-      assert.equal(5, solution.part2([-6, 3, 8, 5, -6]));
+      assert.equal(solution.part2([-6, 3, 8, 5, -6]), 5);
     });
 
     it("First reaches 14 twice", () => {
-      assert.equal(14, solution.part2([7, 7, -2, -7, -4]));
+      assert.equal(solution.part2([7, 7, -2, -7, -4]), 14);
     });
   });
 });
